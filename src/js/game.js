11942fad5d6512702ea359bc6c7472bf85bea453
DanilT7 <dan20001410@gmail.com>
2023-07-16 13:50:58 +0300

    test

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,8 @@
+import { WORDS } from "./consts"
+
 export const startGame = () => {
-    
+    const randomIndex = Math.floor(Math.random()*WORDS.length);
+    const wordToGuess = WORDS[randomIndex]
+    // localStorage.setItem('word', wordToGuess);
+    sessionStorage.setItem('word', wordToGuess);
 }
